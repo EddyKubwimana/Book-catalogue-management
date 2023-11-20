eda3d7b12aabf9ad901bb89d3035a598d410d081
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-11-20 21:22:23 +0000

    first commit

---
+++ Book.java
@@ -0,0 +1,16 @@
+/* This project is meant to simulate the library
+ * @ co-author EddyKubwimana
+ * @ co-author Clovis Mushagalusa Cirubakadhera
+ * @ co-author Muhammed
+ * @ co-author Cajetan
+ * 
+ * 
+ */
+class Book{
+
+    String authorFirstName;
+    String authorLastName;
+
+
+
+}
